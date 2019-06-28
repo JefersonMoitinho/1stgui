bf690bb92b253e66e59fca33924a1f7a262cafd3
Jeferson <jefersonmoitinho20@gmail.com>
2019-06-27 21:01:28 -0300

    Strings

--- src/main/java/br/edu/ifsc/ui/util/Strings.java
+++ src/main/java/br/edu/ifsc/ui/util/Strings.java
@@ -3,13 +3,70 @@ package br.edu.ifsc.ui.util;
 public class Strings {
 
 	// general strings
-	public static final String appTitle = "1stGUI";
+	public static final String appTitle = "Academia";
 
 	// login stage strings
-	public static final String username = "UsuÃ¡rio";
-	public static final String password = "Senha";
+	public static final String username = "Digite usuario";
+	public static final String password = "Digite senha";
 	public static final String loginError = "Erro na autenticaÃ§Ã£o.";
 	public static final String btnLogin = "Entrar";
+
+	// String class Main
+	public static final String lblMain1 = "Olá, bem vindo ";
+	public static final String btnMenuSair = "Sair";
+	public static final String btnMenuAluno = " Gerenciar Aluno";
+	public static final String btnMenuProfessor = "Gerenciar Treinador";
+	public static final String btnMenuRotinaClasse = "Rotina Classe";
+	public static final String btnMenuBiblioteca = "Biblioteca";
+	public static final String btnMenuDefinicoes = "Definições de usuario";
+
+	// String class StudentScream
+	public static final String lblStudentScream = "Menu Aluno";
+	public static final String btnStudentVoltar = "Voltar";
+	public static final String btnStudentCad = "Novo";
+	public static final String btnStudentDel = "Excluir";
+	public static final String message2 = "Aluno cadastrado com sucesso";
+
+	// String class TeaherScream
+	public static final String lblTeacherScream = "Menu Treinador";
+	public static final String btnTeacherVoltar = "Voltar";
+	public static final String btnTeachertCad = "Novo";
+	public static final String btnTeacherDel = "Excluir";
+
+	// String class CadStudent
+	public static final String lblCadStudent = "Cadastrar  Aluno";
+	public static final String btnStudentAdd = "Adicionar";
+	public static final String btnStudentCanc = "Cancelar";
+	public static final String lblNomeStudent = "Nome:";
+	public static final String lblNascStudent = "Data ";
+	public static final String lblRgStudent = "Rg:";
+	public static final String lblCpfStudent = "Cpf:";
+	public static final String lblSexoStudent = "Sexo:";
+	public static final String lblEndereco = "Endereço:";
+	public static final String lblPeso = "Peso:";
+	public static final String lblAltura = "Altura:";
+	public static final String nome = "Nome";
+	public static final String rg = "RG";
+	public static final String cpf = "CPF";
+	public static final String data = "Data";
+	public static final String peso = "Peso";
+	public static final String altura = "Altura";
+	public static final String endereco = "Endereço";
+	
+
+	// String class CadTeacher
+	public static final String lblCadTeacher = "Cadastrar  Treinador";
+	public static final String btnTeacherAdd = "Adicionar";
+	public static final String btnTeacherCanc = "Cancelar";
+	public static final String lblNomeTeacher = "Nome:";
+	public static final String lblNascTeacher = "Data Nascimento:";
+	public static final String lblRgTeacher = "Rg:";
+	public static final String lblCpfTeacher = "Cpf:";
+	public static final String lblSexoTeacher = "Sexo:";
+	public static final String message3 = "Treinador cadastrado com sucesso";
+	public static final String message4 = "As alteracoes nao foram salvas!";
+	public static final String message5 = "Deseja descarta-las?";
+	public static final String lblArea = "Area de especialização:";
 
 	// main stage strings
 	public static final String lblMain = "OlÃ¡";
